d86cdee534aaf9ea79969dac11ac83d7aca4173f
axentamx <finanzas@axentaexpert.com>
2025-10-02 11:06:56 -0600

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,6 @@
+// Configuración del sitio Axenta
+// Para activar Formspree, reemplace 'FORM_ID' por su ID real (ej. 'xqkrlgwy') y deje el endpoint como está.
+window.AXENTA_CONFIG = {
+  contactEmail: 'finanzas@axentaexpert.com',
+  formspreeEndpoint: '' // 'https://formspree.io/f/FORM_ID'
+};
